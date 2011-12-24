806b916e456e83f3a1e82deda6b5bc0c93674e17
Ilias Stamatis <stamatis.iliass@gmail.com>
2011-12-24 20:11:17 +0200

    remove class

--- ffmulticonverter/pyqttools.py
+++ ffmulticonverter/pyqttools.py
@@ -22,99 +22,78 @@ A class with some useful methods to automate some parts of ui creation.
 
 from PyQt4.QtGui import QWidget, QLayout, QAction, QLineEdit, QSpacerItem
 from PyQt4.QtCore import QSize
+ 
+def add_to_layout(layout, *items):
+    """Add items to QVBox and QHBox layouts easily.
+        
+    Keyword arguments:
+    layout -- a layout (QVBox or QHBox)
+    *items -- list with items to be added
+    """
+    for item in items:
+        if isinstance(item, QWidget):
+            layout.addWidget(item)
+        elif isinstance(item, QLayout):
+            layout.addLayout(item)
+        elif isinstance(item, QSpacerItem):
+            layout.addItem(item)
+        elif item is None:
+            layout.addStretch()
+    return layout
 
-class Tools:
-    def __init__(self):
-        pass
+def add_to_grid(layout, *items):
+    """Add items to a QGrid layout easily.
         
-    def add_to_layout(self, layout, *items):
-        """Add items to QVBox and QHBox layouts easily.
-        
-        Keyword arguments:
-        layout -- a layout (QVBox or QHBox)
-        *items -- list with items to be added
-        """
-        for item in items:
+    Keyword arguments:
+    layout -- a QGridLayout
+    *items -- list with lists of items to be added.
+              items in the same list will be added to the same line
+    """
+    # for know it just only adds only 1 item per cell.
+    x = 0
+    for _list in items:
+        y = 0
+        for item in _list:
             if isinstance(item, QWidget):
-                layout.addWidget(item)
+                layout.addWidget(item, x, y)
             elif isinstance(item, QLayout):
-                layout.addLayout(item)
+                layout.addLayout(item, x, y)
             elif isinstance(item, QSpacerItem):
-                layout.addItem(item)
-            elif item is None:
-                layout.addStretch()
-        return layout
+                layout.addItem(item, x, y)
+            y += 1
+        x += 1
+    return layout
 
-    def add_to_grid(self, layout, *items):
-        """Add items to a QGrid layout easily.
+def createAction(parent, text, slot, shortcut=None, tip=None):
+    """Creates Actions.
         
-        Keyword arguments:
-        layout -- a QGridLayout
-        *items -- list with items to be added
-        """
-        # for know it just only adds only 1 item per cell.
-        x = 0
-        for _list in items:
-            y = 0
-            for item in _list:
-                if isinstance(item, QWidget):
-                    layout.addWidget(item, x, y)
-                elif isinstance(item, QLayout):
-                    layout.addLayout(item, x, y)
-                elif isinstance(item, QSpacerItem):
-                    layout.addItem(item, x, y)
-                y += 1
-            x += 1
-        return layout
-
-    def createAction(self, parent, text, slot, shortcut=None, tip=None):
-        """Creates Actions.
+    Keyword arguments:
+    parent -- parent
+    text -- action's text
+    slot -- slot to connect action
+    shortcut -- action's shortcut
+    tip -- action's tip to display
         
-        Keyword arguments:
-        parent -- parent
-        text -- action's text
-        slot -- slot to connect action
-        shortcut -- action's shortcut
-        tip -- action's tip to display
+    Returns: QAction
+    """
+    action = QAction(text, parent)
+    if shortcut is not None:
+        action.setShortcut(shortcut)
+    if tip is not None:
+        action.setToolTip(tip)
+        action.setStatusTip(tip)
+    action.triggered.connect(slot)
+    return action    
         
-        Returns: QAction
-        """
-        action = QAction(text, parent)
-        if shortcut is not None:
-            action.setShortcut(shortcut)
-        if tip is not None:
-            action.setToolTip(tip)
-            action.setStatusTip(tip)
-        action.triggered.connect(slot)
-        return action    
-        
-    def addActions(self, target, actions):
-        """Adds actions to menus.
-        
-        Keyword arguments:
-        target -- Menu to add action
-        actions -- list with actions to add
-                   Add separator for None in list
-        """
-        for action in actions:
-            if action is None:
-                target.addSeparator()
-            else:
-                target.addAction(action)
-
-    def create_LineEdit(self, maxsize, validator, maxlength):
-        """Creates a lineEdit
-        
-        Keyword arguments:
-        maxsize -- maximum size
-        validator -- a regular expression to be added as validator
-        maxlength - maximum length
-        """        
-        lineEdit = QLineEdit()
-        if maxsize is not None:
-            lineEdit.setMaximumSize(QSize(maxsize[0], maxsize[1]))
-        if validator is not None:
-            lineEdit.setValidator(validator)
-        if maxlength is not None:
-            lineEdit.setMaxLength(maxlength)
-        return lineEdit
+def addActions(target, actions):
+    """Adds actions to menus.
+    
+    Keyword arguments:
+    target -- Menu to add action
+    actions -- list with actions to add                
+    """
+    for action in actions:
+        if action is None:
+            target.addSeparator()
+        else:
+            target.addAction(action)

9db940a485f89746fc87562a015b52149f2268b2
Ilias Stamatis <stamatis.iliass@gmail.com>
2013-09-12 05:59:38 +0300

    rewrite test_dialogs.py

--- test/test_dialogs.py
+++ test/test_dialogs.py
@@ -10,30 +10,21 @@ from ffmulticonverter.preferences_dlg import Preferences
 from ffmulticonverter.progress import Progress
 
 
-def test_AboutDialog():
-    return AboutDialog('About Dialog', ':/ffmulticonverter.png', 'Authors',
-                       'Translators')
-
-def test_ShowPresetsDialog():
-    return ShowPresets()
-
-def test_PreferencesDialog():
-    return Preferences(test=True)
-
-def test_ProgressDialog():
-    return Progress([], '', '', False, '', False, False, None, test=True)
-
-
 def main():
     app = QApplication(sys.argv)
-    # uncomment the dialog you wish to test
-    dialog = test_AboutDialog()
-    #dialog = test_ShowPresetsDialog()
-    #dialog = test_PreferencesDialog()
-    #dialog = test_ProgressDialog()
-    dialog.show()
-    app.exec_()
 
+    dlg_about = AboutDialog('About Dialog', ':/ffmulticonverter.png', 'Authors', 'Translators')
+    dlg_showpresets = ShowPresets()
+    dlg_preferences = Preferences(test=True)
+    dlg_progress = Progress([], '', '', False, '', False, False, None, test=True)
+
+    # uncomment the dialog you wish to test
+    dlg_about.show()
+    #dlg_showpresets.show()
+    #dlg_preferences.show()
+    #dlg_progress.show()
+
+    app.exec_()
 
 if __name__ == '__main__':
     main()

4fb999e6bec7f6fde540c696e28b0441c45e2409
Ilias Stamatis <stamatis.iliass@gmail.com>
2014-01-21 20:07:46 +0200

    update copyright lines

--- ffmulticonverter/preferences_dlg.py
+++ ffmulticonverter/preferences_dlg.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013 Ilias Stamatis <stamatis.iliass@gmail.com>
+# Copyright (C) 2011-2014 Ilias Stamatis <stamatis.iliass@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

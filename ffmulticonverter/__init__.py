b92ad3f891c863aaf1bb347fb30a06944b03a955
Ilias Stamatis <stamatis.iliass@gmail.com>
2014-02-23 01:44:31 +0200

    version 1.6.1-a1

--- ffmulticonverter/__init__.py
+++ ffmulticonverter/__init__.py
@@ -5,8 +5,8 @@ GUI File Format Converter
 # version
 __major__ = 1  # for major interface/format changes
 __minor__ = 6  # for minor interface/format changes
-__release__ = 0  # for tweaks, bug-fixes, or development
-__prerelease__ = "" # alpha, beta, rc etc.
+__release__ = 1  # for tweaks, bug-fixes, or development
+__prerelease__ = "-a1" # alpha, beta, rc etc.
 
 # package information
 __name__ = "ffmulticonverter"

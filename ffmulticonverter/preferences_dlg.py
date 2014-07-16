34a37320a87528e6f023e448c99fc6ca3962ccdb
Ilias Stamatis <stamatis.iliass@gmail.com>
2014-07-17 01:45:30 +0300

    remove unused os import

--- ffmulticonverter/preferences_dlg.py
+++ ffmulticonverter/preferences_dlg.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from PyQt4.QtCore import QSettings
 from PyQt4.QtGui import (
         QDialog, QDialogButtonBox, QFileDialog, QLabel, QLineEdit,

79df00824ee57f252fb1c36853532bee16251753
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-02-28 17:17:09 +0200

    remove sudo commands from uninstall script
    not all users have sudo installed

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 # ffmulticonverter uninstall script
-# Usage: ./uninstall [--all]
 
-for i in $(sudo find /usr -path "*ffmulticonverter*")
-do
-	sudo rm -rf $i
+for i in $(find /usr -path "*ffmulticonverter*"); do
+	rm -rf $i
 done
-
-if [[ "$#" -eq 1 && "$1" = "--all" ]]; then
-	# remove configuration files as well
-	rm -rf ~/.config/ffmulticonverter
-fi
